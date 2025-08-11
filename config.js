dfb57fb296aab766ce7d2ba3cb81190e314140d3
Cyber-kavi <kd241039@gmail.com>
2025-08-11 14:12:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "R6dkRZjC#W-0VIAjSsdIGxjxeDe7gVx8oDLtg-tygHDqcSJPXlb8", // session id  
+SESSION_ID: process.env.SESSION_ID || "06d3BRwa#IikC1jdPnaz2DJo8LpLqqVIE2b89S7meYRbcqo3rH40", // session id 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public", //public , groups , inbox , channel
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
